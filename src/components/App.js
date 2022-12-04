687ea51c50c1a40ee0ce24648b8ac5c405c2ab7f
Fe195 <114395573+Fe195@users.noreply.github.com>
2022-12-04 13:31:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,11 @@ function App()
 	<div id="main">
 	//Do not alter main div
 	//Please do not alter the functional component as tests depend on the type of component.
+		<text area="text" id="task" required>
+		<ol>
+		<li>Buy milk</li>
+		<li>Buy vegetable</li>
+		<button type="submit" value="submit">Submit</button>
 	</div>
 	);
 }
